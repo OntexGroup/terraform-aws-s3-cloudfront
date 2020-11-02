f66d17d9ab5741b4c81b1cbb2c2f14d35145ca3b
Alban MUSSI <alban_mussi@uptimemanagers.fr>
2020-11-02 15:31:42 +0100

    feat: add cf distribution id to output

--- front.tf
+++ front.tf
@@ -226,6 +226,10 @@ output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
+
 output "s3_user_access_key_id" {
   value     = aws_iam_access_key.s3_front_user_key.id
 }
